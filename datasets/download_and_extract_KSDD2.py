c859f3f68ab4f2977424a185adde65ecaedb241b
LAKY911 <44507644+LAKY911@users.noreply.github.com>
2024-02-23 20:11:06 +0100

    First commit

---
+++ datasets/download_and_extract_KSDD2.py
@@ -0,0 +1,12 @@
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+
+if __name__ == "__main__":
+
+    zipurl = 'http://go.vicos.si/kolektorsdd2'
+
+    with urlopen(zipurl) as zipresp:
+        with ZipFile(BytesIO(zipresp.read())) as zfile:
+            zfile.extractall('./datasets/')
+    print('Done downloading and extracting KolektorSDD2 dataset.')
