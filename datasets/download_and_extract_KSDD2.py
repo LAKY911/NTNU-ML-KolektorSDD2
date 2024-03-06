b864d085bcf53586baeae53e907ac5698efdf25d
LAKY911 <lukas.vitek911@gmail.com>
2024-03-06 16:53:12 +0100

    Fixed SSLError in download_and_extract_KSDD2.py

--- datasets/download_and_extract_KSDD2.py
+++ datasets/download_and_extract_KSDD2.py
@@ -2,6 +2,12 @@ from io import BytesIO
 from urllib.request import urlopen
 from zipfile import ZipFile
 
+# **If URLError -> uncomment the following two lines**
+# import ssl
+# ssl._create_default_https_context = ssl._create_unverified_context
+# 
+# **
+
 if __name__ == "__main__":
 
     zipurl = 'http://go.vicos.si/kolektorsdd2'
